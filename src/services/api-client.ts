acc6624a9db222bc542fd05a6b84dcbf121ca875
liucuxiu <xliucuxiux@gmail.com>
2023-07-24 14:31:31 +0700

    refactor gerne hook with react query

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,28 +1,8 @@
 import axios from "axios";
 
-const axiosInstance = axios.create({
+export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
     key: "e1103af34bc94fa1b5c6d04f1b39fec6",
   },
 });
-
-class ApiClient<T> {
-  endpoint: string;
-
-  constructor(endpoint: string) {
-    this.endpoint = endpoint;
-  }
-
-  getAll = () => {
-    return axiosInstance.get<T[]>(this.endpoint)
-      .then(res => res.data);
-  }
-
-  post = (data: T) => {
-    return axiosInstance.post<T>(this.endpoint, data)
-      .then(res => res.data);
-  }
-}
-
-export default ApiClient;
