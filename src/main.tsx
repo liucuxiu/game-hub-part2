1b33702fbf01ad8c5eb998cb8948dfbcf426f31d
liucuxiu <xliucuxiux@gmail.com>
2023-07-25 08:48:31 +0700

    refactor: simplify game query

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from './App'
 import theme from './theme'
 import './index.css'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -24,7 +25,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
       <ColorModeScript initialColorMode={theme.config.initialColorMode}/>
       <QueryClientProvider client={queryClient}>
         <App/>
-
+        <ReactQueryDevtools initialIsOpen={false}/>
       </QueryClientProvider>
 
     </ChakraProvider>
