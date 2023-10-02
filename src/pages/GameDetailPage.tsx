e62eed6c5f4645cf8521aeae68f19e5bbb288098
liucuxiu <xliucuxiux@gmail.com>
2023-10-02 10:20:16 +0700

    feat: fetch game detail

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,7 +1,19 @@
+import { useParams } from 'react-router-dom';
+import useGame from '../hooks/useGame';
+import { Heading, Spinner, Text } from '@chakra-ui/react';
+
 const GameDetailPage = () => {
+  const { slug } = useParams();
+  const { data: game, isLoading, error } = useGame(slug!);
+
+  if (isLoading) return <Spinner/>;
+
+  if (error || !game) return <div>error</div>;
+
   return (
     <div>
-      <h1>Game Detail Page</h1>
+      <Heading>{game.name}</Heading>
+      <Text>{game.description_raw}</Text>
     </div>
   );
 }
