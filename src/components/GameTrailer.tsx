d87cc971395707da3374cbb69c47e9f91a62ce71
liucuxiu <xliucuxiux@gmail.com>
2023-10-02 14:33:09 +0700

    feat: build game trailer

---
+++ src/components/GameTrailer.tsx
@@ -0,0 +1,27 @@
+import useTrailers from '../hooks/useTrailers';
+
+interface Props {
+  gameId: number;
+}
+
+const GameTrailer = ({ gameId }: Props) => {
+  const { data, isLoading, error } = useTrailers(gameId);
+
+  if (isLoading) return null;
+
+  if (error) throw error;
+
+  const first = data?.results[0]
+  if (!first) return null;
+
+  return (
+    <>
+      <video src={first.data[480]}
+             poster={first.preview}
+             controls
+      />
+    </>
+  )
+}
+
+export default GameTrailer;
